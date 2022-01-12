90d9a3dd4296f3628923bf1c52282e24c2c6ce30
adrianfocke <afocke@pm.me>
2022-01-12 18:57:02 +0100

    Update

--- components/Menu.js
+++ components/Menu.js
@@ -3,9 +3,8 @@ import Link from 'next/link'
 const Menu = ({likes}) => {
   return(
     <nav className="flexbox margin-top margin-bottom">
-      <Link href="/"><a title="Back to home">Swipe for Change!</a></Link>
+      <Link href="/"><a title="Back to home">Activie</a></Link>
       <ul className="flexbox link-collection margin-none padding-none">
-        <li><Link href="/"><a title="Back to home">Home</a></Link></li>
         <li><Link href="/profiles"><a title="Navigate to profiles">Profile</a></Link></li>
         <li><Link href="/likes"><a title="Navigate to likes">Likes</a></Link></li>
         <li><Link href="/faq"><a title="Navigate to faq">FAQ</a></Link></li>
