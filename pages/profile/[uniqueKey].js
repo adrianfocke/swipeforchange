154ddbb99a0c0400edbb84d5fe5418d751528a1c
adrianfocke <afocke@pm.me>
2022-01-24 14:29:14 +0100

    React hierarchy I do not understand

--- pages/profile/[uniqueKey].js
+++ pages/profile/[uniqueKey].js
@@ -53,11 +53,12 @@ export async function getStaticProps({ params }) {
 
 const Details = ({ profile }) => {
 
-  // if(!profile) return <Skeleton />
+
 
   const { uniqueKey, name, location, slogan, about, goals, tags, firstImage, secondImage, thirdImage, fourthImage, longDescription, contactName, contactStatement, contactImage, events, contactNumber,  contactEmail, contactLink} = profile.fields
 
   const [shownImage, setShownImage] = useState("firstImage")
+
   const toggleImage = () => {
     if (shownImage === "firstImage") {
       setShownImage("secondImage");
@@ -67,6 +68,8 @@ const Details = ({ profile }) => {
       setShownImage("firstImage")
     }
   }
+
+  if(!profile) return <Skeleton />
 
   return (
     <div className="card margin-bottom-double">
